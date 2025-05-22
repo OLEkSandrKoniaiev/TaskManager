8bfcbca97b8852f4bc57b230dcacad5d42ea6dd8
OLEkSandrKoniaiev <128646916+OLEkSandrKoniaiev@users.noreply.github.com>
2025-05-22 23:54:53 +0300

    add fields validation for User model and start work with JWT

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema(
     {
@@ -9,6 +10,9 @@ const userSchema = new mongoose.Schema(
             lowercase: true,
             unique: true,
             trim: true,
+            match: [/^[a-zA-ZА-Яа-яЇїІіЄєҐґ0-9\s\-_.]+$/, 'Username can only contain Latin/Cyrillic letters, numbers, spaces, hyphens, underscores, and dots'],
+            minlength: [3, 'Username must be at least 3 characters long'],
+            maxlength: [30, 'Username cannot be more than 30 characters long'],
         },
         email: {
             type: String,
@@ -16,11 +20,19 @@ const userSchema = new mongoose.Schema(
             lowercase: true,
             unique: true,
             trim: true,
+            match: [
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                'Please enter a valid email address'
+            ],
         },
         password: {
             type: String,
             required: [true, 'Password is required'],
-            minlength: 8,
+            minlength: [8, 'Password must be at least 8 characters long'],
+            match: [
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,}$/,
+                'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.'
+            ],
         },
         role: {
             type: String,
@@ -37,20 +49,29 @@ const userSchema = new mongoose.Schema(
     },
 );
 
+userSchema.index({username: 1, email: 1});
+
 // Have hashed password before saving
 userSchema.pre('save', async function (next) {
+    // Check if a password field is modified or if it's a new document
     if (!this.isModified('password')) {
-        next();
+        return next(); // Corrected to return next()
     }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
     next();
 });
 
-
 // Method for checking password
 userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Method for generating JWT token
+userSchema.methods.getSignedJwtToken = function () {
+    return jwt.sign({id: this._id, role: this.role}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE
+    });
 };
 
 const User = mongoose.model('User', userSchema);
