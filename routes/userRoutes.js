274901c432a395c146e34cd3745fb7006af18895
OLEkSandrKoniaiev <128646916+OLEkSandrKoniaiev@users.noreply.github.com>
2025-05-23 20:25:57 +0300

    add middleware for role access

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const {getUsers, registerUser, loginUser, refreshToken} = require('../controllers/userController');
+const {protect} = require('../middlewares/authMiddleware');
+const authorizeRoles = require('../middlewares/authorizeRolesMiddleware');
 
-router.get('/', getUsers);
+router.get('/', protect, authorizeRoles('admin'), getUsers);
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/refresh-token', refreshToken);
